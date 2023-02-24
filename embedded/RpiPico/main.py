469b5c075f289a15f8bae9e83c723b2c59ed73b0
dan <51975338+dakschnitzer@users.noreply.github.com>
2023-02-23 19:25:32 -0500

    able to generate predictions in rise/set times and compare to actual

--- embedded/RpiPico/main.py
+++ embedded/RpiPico/main.py
@@ -42,33 +42,41 @@ def find_rise_set(lat, lon, year, month, day, hour, minute, second, ra, dec):
     ha_set = math.acos((-math.sin(lat) * math.sin(dec)) / (math.cos(lat) * math.cos(dec)))
     ha_set = math.degrees(ha_set)
     #calculate UT of sun in south - can't figure out if a negative number means it's day before or day after.
-    utss = (ra * 15 - gst0 * 15 - lon) / 15.0417
-    if utss >= 0:
-        utss = datetime(year, month, day, int(utss), int((utss * 60) % 60), int((utss * 3600 % 60)))
-    elif utss < 0:
-        utss = utss + 24
-        utss = datetime(year, month, day, int(utss), int((utss * 60) % 60), int((utss * 3600 % 60))) - timedelta(days = 1)
+    utss_hr = (ra * 15 - gst0 * 15 - lon) / 15.047
+    print('gst0: ', gst0)
+    print('utss_hr: ', utss_hr)
+    print('ra: ', ra)
+    print('ra-gst0: ', ra-gst0)
     
+    utss = datetime(year, month, day) + timedelta(hours = utss_hr)
     
+    # if utss >= 0:
+    #     utss = datetime(year, month, day, int(utss), int((utss * 60) % 60), int((utss * 3600 % 60)))
+    # elif utss < 0:
+    #     utss = utss + 24
+    #     utss = datetime(year, month, day, int(utss), int((utss * 60) % 60), int((utss * 3600 % 60))) 
+    #     #- timedelta(days = 1)
+    
+    print('utss timestamp: ', utss)
     #local rise time in UTC
     lrise = utss - timedelta(hours = ha_set / 15)  
     #local set time in UTC
     lset = utss + timedelta(hours = ha_set / 15)
     
     
-    print('gst- in hours is', gst0,'utss in hours is', utss, 'ha_set in degrees is', ha_set, 'lrise is', lrise, 'lset is', lset, sep='\n')
+    # print('gst- in hours is', gst0,'utss in hours is', utss, 'ha_set in degrees is', ha_set, 'lrise is', lrise, 'lset is', lset, sep='\n')
     
-    return lrise, lset
+    return lrise, lset, ra, gst0, utss_hr
 
 
-now = datetime.utcnow()
-year = now.year
-month = now.month
-day = now.day
+# now = datetime.utcnow()
+# year = now.year
+# month = now.month
+# day = now.day
 
 
-lat = 38.9072
-lon = -77.0369
+# lat = 38.9072
+# lon = -77.0369
 
-ra = 5.0266
-dec = 0.4406786676497294
+# ra = 5.0266
+# dec = 0.4406786676497294
--- embedded/RpiPico/prediction_analysis.py
+++ embedded/RpiPico/prediction_analysis.py
@@ -16,55 +16,70 @@ lon = -77.0369
 def predict_accuracy(lat, lon, start_year, start_month, start_day, start_hour, start_minute, start_second, name, n_months):
     #check the accuracy of the rise time predicted by the rise_set_list function against a list of actual rise times from skyfield
     
-    #generate list of daily actual rise, set times from skyfield
-    rise_set_actual = rise_set_list(lat, lon, start_year, start_month, start_day, start_hour, start_minute, start_second, name, n_months)
-    rise_set_value = [column[1] for column in rise_set_actual]
-    rise_set_value = numpy.array(rise_set_value)
-    rise_set_time = [column[0] for column in rise_set_actual]
-    rise_set_time = numpy.array(rise_set_time)
-    
-    rise_times_actual = rise_set_time[numpy.where(rise_set_value == 1)]
-    rise_times_actual = [datetime.strptime(date, '%Y/%m/%d %H:%M:%S') for date in rise_times_actual]
-    
-    
-    set_times_actual = rise_set_time[numpy.where(rise_set_value == 0)]
-    set_times_actual = [datetime.strptime(date, '%Y/%m/%d %H:%M:%S') for date in set_times_actual]
-    
-    
+    #generate list of daily predicted rise times from ra, dec
     start_date = datetime(start_year, start_month, start_day, start_hour, start_minute, start_second)
     
     n_days = start_date + relativedelta(months = n_months) - start_date
     n_days = n_days.days
     
-    #generate list of daily predicted rise times from ra, dec
     radec = radec_list(start_year, start_month, start_day, start_hour, start_minute, start_second, name, 1, n_days, 0)
-    lrise = [[0 for i in range(1)] for j in range(len(radec))]
-    lset = [[0 for i in range(1)] for j in range(len(radec))]
-
+    predict_times = [[0 for i in range(5)] for j in range(len(radec))]
+    
+    #rise_times_p and set_times_p will be the predicted rise and set times.
     for i in range(len(radec)):
         date = start_date + timedelta(days = i)
         
         ra = radec[i][1]
         dec = radec[i][2]
         
-        lrise[i] = find_rise_set(lat, lon, date.year, date.month, date.day, date.hour, date.minute, date.second, ra, dec)[0]
-        lset[i] =  find_rise_set(lat, lon, date.year, date.month, date.day, date.hour, date.minute, date.second, ra, dec)[1]
+        predict_times[i] = find_rise_set(lat, lon, date.year, date.month, date.day, date.hour, date.minute, date.second, ra, dec)
+    
+    rise_times_p = [column[0] for column in predict_times]
+    set_times_p = [column[1] for column in predict_times]
+    ra = [column[2] for column in predict_times]
+    gst0 = [column[3] for column in predict_times] 
+    utss = [column[4] for column in predict_times]
+    
+    #next, generate list of daily actual rise, set times from skyfield
+    #first, start on the same day as the predicted rise time
+    rise_set_actual = rise_set_list(lat, lon, rise_times_p[0].year, rise_times_p[0].month, rise_times_p[0].day, 0, 0, 0, name, n_months)
+    rise_set_value = [column[1] for column in rise_set_actual]
+    rise_set_value = numpy.array(rise_set_value)
+    rise_set_time = [column[0] for column in rise_set_actual]
+    rise_set_time = numpy.array(rise_set_time)
+    
+    #list of daily rise times
+    rise_times_actual = rise_set_time[numpy.where(rise_set_value == 1)]
+    rise_times_actual = [datetime.strptime(date, '%Y/%m/%d %H:%M:%S') for date in rise_times_actual]
+    
+    #list of daily set times
+    set_times_actual = rise_set_time[numpy.where(rise_set_value == 0)]
+    set_times_actual = [datetime.strptime(date, '%Y/%m/%d %H:%M:%S') for date in set_times_actual]
+    
+    
+    #calculate the difference between predicted and actual rise
+    rise_diff = [(predict - actual).total_seconds() for (predict, actual) in zip(rise_times_p, rise_times_actual)]
+    print(rise_diff)
+    
+    return rise_diff, ra, gst0, utss
     
 
-    return rise_times_actual
-    
+rise_diff = predict_accuracy(lat, lon, 2023, 2, 1, 0, 0, 0, 'mercury', 12)
+ra = rise_diff[1]
+gst0 = rise_diff[2]
+utss = rise_diff[3]
+rise_diff = rise_diff[0]
 
-rise_times = predict_accuracy(lat, lon, 2023, 2, 1, 0, 0, 0, 'jupiter', 1)
 
 # dates_list = [datetime.strptime(date, '%Y/%m/%d %H:%M:%S') for date in rise_times]
 
 # (dates_list[1]-dates_list[0]).total_seconds()
 
-start_year = 2023
-start_month = 2
-start_day = 1
-start_hour = 0 
-start_minute = 0
-start_second = 0 
-name = 'jupiter' 
-n_months = 1
+# start_year = 2023
+# start_month = 2
+# start_day = 1
+# start_hour = 0 
+# start_minute = 0
+# start_second = 0 
+# name = 'jupiter' 
+# n_months = 1
